25bb7df6041b53d803954e35fba05216c9769bcb
Branislav Blazek <blazek@pap.sk>
2022-02-17 15:35:01 +0100

    styles

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,8 @@
 const Footer = () => {
     return ( 
-        <div className="footer">
-            Copyright 2022 Branisla0
-        </div>
+        <footer className="footer">
+            Copyright 2022 - Branisla0
+        </footer>
      );
 }
  
--- pages/index.js
+++ pages/index.js
@@ -6,11 +6,11 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div>
-      <h1>AHOJ SVET</h1>
-      <p>lorem ipsum dolor sit amet consectetur adipiscing elit laoreet varius ultrices maecenas euismod curabitur odio ullamcorper leo sociosqu congue cursus neque orci sodales primis porttitor cubilia dis gravida a quis dictumst scelerisque etiam nisl tortor fames purus placerat penatibus curae lectus tristique parturient nec nisi per aenean lobortis sem litora</p>
-      <p>lorem ipsum dolor sit amet consectetur adipiscing elit laoreet varius ultrices maecenas euismod curabitur odio ullamcorper leo sociosqu congue cursus neque orci sodales primis porttitor cubilia dis gravida a quis dictumst scelerisque etiam nisl tortor fames purus placerat penatibus curae lectus tristique parturient nec nisi per aenean lobortis sem litora</p>
+      <h1 className={styles.title}>Homepage</h1>
+      <p className={styles.text}>lorem ipsum dolor sit amet consectetur adipiscing elit laoreet varius ultrices maecenas euismod curabitur odio ullamcorper leo sociosqu congue cursus neque orci sodales primis porttitor cubilia dis gravida a quis dictumst scelerisque etiam nisl tortor fames purus placerat penatibus curae lectus tristique parturient nec nisi per aenean lobortis sem litora</p>
+      <p className={styles.text}>lorem ipsum dolor sit amet consectetur adipiscing elit laoreet varius ultrices maecenas euismod curabitur odio ullamcorper leo sociosqu congue cursus neque orci sodales primis porttitor cubilia dis gravida a quis dictumst scelerisque etiam nisl tortor fames purus placerat penatibus curae lectus tristique parturient nec nisi per aenean lobortis sem litora</p>
       <Link href="/ninjas">
-        <a>See Ninjas Listing</a>
+        <a className={styles.tbn}>See Ninjas Listing</a>
       </Link>
     </div>
   )
